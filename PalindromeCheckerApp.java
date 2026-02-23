04f17e2e66632f9664cad1cd749aeebbf75949cd
rootbrites <bhaskara.skvamshitha@gmail.com>
2026-02-23 09:28:41 +0530

    Base application setup

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App!");
+
+        System.out.println("Application: Palindrome Checker");
+        System.out.println("Version: 1.0.0");
+        System.out.println("Status: Initialized and Ready.");
+
+    }
+}
